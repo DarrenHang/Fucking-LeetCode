cc17f7b6e64a755e3a44b274b7fc34e91c26daa7
Darren <305644149@qq.com>
2021-06-24 10:53:05 +0800

    leetcode12 整数转罗马数字

--- app/src/main/java/com/darren/leetcode/leetcode12/LeetCode.java
+++ app/src/main/java/com/darren/leetcode/leetcode12/LeetCode.java
@@ -1,5 +1,7 @@
 package com.darren.leetcode.leetcode12;
 
+import java.util.HashMap;
+
 /**
  * date  6/23/21  5:32 PM
  * author  DarrenHang
@@ -7,10 +9,11 @@ package com.darren.leetcode.leetcode12;
 class LeetCode {
 
     public static void main(String[] args) {
-        System.out.println(intToRoman(40));
+        System.out.println(intToRoman_(40));
     }
 
 
+    //暴力破解
     public static String intToRoman(int num) {
 
         StringBuilder sb = new StringBuilder();
@@ -92,5 +95,42 @@ class LeetCode {
 
         return sb.toString();
     }
+
+
+    //递归
+    public static String intToRoman_(int num) {
+        if (num >= 1000) return "M" + intToRoman_(num -= 1000);
+        if (num >= 900) return "CM" + intToRoman_(num -= 900);
+        if (num >= 500) return "D" + intToRoman_(num -= 500);
+        if (num >= 400) return "CD" + intToRoman_(num -= 400);
+        if (num >= 100) return "C" + intToRoman_(num -= 100);
+        if (num >= 90) return "XC" + intToRoman_(num -= 90);
+        if (num >= 50) return "L" + intToRoman_(num -= 50);
+        if (num >= 40) return "XL" + intToRoman_(num -= 40);
+        if (num >= 10) return "X" + intToRoman_(num -= 10);
+        if (num >= 9) return "IX" + intToRoman_(num -= 9);
+        if (num >= 5) return "V" + intToRoman_(num -= 5);
+        if (num >= 4) return "IV" + intToRoman_(num -= 4);
+        if (num >= 1) return "I" + intToRoman_(num -= 1);
+        else return "";
+    }
+
+    //贪心算法
+    public static String intToRoman__(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romas = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            for (int i = 0; i < nums.length; i++) {
+                if (num >= nums[i]) {
+                    num -= nums[i];
+                    sb.append(romas[i]);
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 
 }
