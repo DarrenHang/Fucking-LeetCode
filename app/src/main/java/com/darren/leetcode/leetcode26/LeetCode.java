28e8548c7c697159407d2bab945c2c28989d8617
Darren <305644149@qq.com>
2021-07-02 09:23:40 +0800

    leetcode26 删除有序数组中的重复项

--- app/src/main/java/com/darren/leetcode/leetcode26/LeetCode.java
+++ app/src/main/java/com/darren/leetcode/leetcode26/LeetCode.java
@@ -11,15 +11,19 @@ class LeetCode {
         System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
     }
 
+    //解题思路
+    //从第一个元素开始便利
+    //区分 ++current 和 current++
+    // current = 1 int a = ++current a = 2 current = 2
+    // current = 1 int b = current++ b = 1 current = 2
     public static int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return 1;
         int length = nums.length;
         int current = 0;
-        for (int i = 0; i < length; i++) {
+        for (int i = 1; i < length; i++) {
             if (nums[i] != nums[current]) {
-                current++;
-                nums[current] = nums[i];
+                nums[++current] = nums[i];
             }
         }
         return current + 1;
