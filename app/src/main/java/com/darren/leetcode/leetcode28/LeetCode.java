502053e0f85d0700106f60654a2ca41305cd81f4
Darren <305644149@qq.com>
2021-07-02 09:33:01 +0800

    leetcode29 实现 strStr()

--- app/src/main/java/com/darren/leetcode/leetcode28/LeetCode.java
+++ app/src/main/java/com/darren/leetcode/leetcode28/LeetCode.java
@@ -11,15 +11,21 @@ class LeetCode {
         strStr("abc","c");
     }
 
+    //解题思路
     public static int strStr(String haystack, String needle) {
+        //当 needle 为空或者两个字符串相等时返回0
         if (needle.isEmpty() || haystack.equals(needle)) return 0;
+        //当 haystack 为空时返回 -1
         if (haystack.isEmpty()) return -1;
         int hLength = haystack.length();
         int nLength = needle.length();
+        //当 haystack 长度小于 needle 时返回 -1
         if (hLength < nLength) return -1;
         int pos = 0;
+        //通过指针移动元素
         while (pos < hLength - nLength + 1) {
             String str = haystack.substring(pos, pos + nLength);
+            //如果找到返回索引
             if (str.equals(needle)) {
                 return pos;
             }
